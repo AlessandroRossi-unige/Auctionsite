87bf7b5766d72fdb5d60977885145183aa9d272c
AlessandroRossi-unige <72498335+AlessandroRossi-unige@users.noreply.github.com>
2021-05-15 12:22:12 +0200

    Add project files.

---
+++ Bindings.cs
@@ -0,0 +1,13 @@
+﻿using AuctionSiteLib;
+using Ninject.Modules;
+using TAP2018_19.AuctionSite.Interfaces;
+
+
+public class Bindings : NinjectModule
+{
+    public override void Load()
+    { 
+        Bind<ISiteFactory>().To<AuctionSiteLib.SiteFactory>();
+    }
+}
+
---
+++ SiteFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using TAP2018_19.AlarmClock.Interfaces;
+using TAP2018_19.AuctionSite.Interfaces;
+
+namespace AuctionSiteLib
+{
+    public class SiteFactory: ISiteFactory
+
+    {
+        public void Setup(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetSiteNames(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSiteOnDb(string connectionString, string name, int timezone, int sessionExpirationTimeInSeconds,
+            double minimumBidIncrement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTheTimezoneOf(string connectionString, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISite LoadSite(string connectionString, string name, IAlarmClock alarmClock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
